9b91351e14e3912c455a7072cb20475abe3eeb4f
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2018-10-07 01:31:09 +0900

    feat: :tada: inquirerで試験実装
    close #3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
+import inquirer from 'inquirer';
+import longest from 'longest';
+import rightPad from 'right-pad';
 import searchAndLoad from './searchAndLoad';
 
-const getConfigQuestions = async () => {
+(async () => {
   const { config } = await searchAndLoad();
-  return config.questions;
-};
+  let { questions } = config;
 
-getConfigQuestions();
+  questions = questions.map(question => {
+    if (!question.choices) return question;
+
+    // longestにわたす配列作成
+    const choiceNameArray = question.choices.map(choice => choice.name);
+
+    // nameとdescriptionを結合させるため
+    const combine = question.choices.map(choice => {
+      // choicesで一番長いnameの文字数を計算 + 余白
+      const choiceNameLength = longest(choiceNameArray).length + 1;
+
+      return {
+        name: `${rightPad(choice.name, choiceNameLength)} ${
+          choice.description
+        }`,
+        value: choice.name,
+      };
+    });
+
+    // eslint-disable-next-line no-param-reassign
+    question.choices = combine;
+
+    return question;
+  });
+  inquirer.prompt(questions).then(answers => {
+    console.log(answers.prefix);
+  });
+})();
