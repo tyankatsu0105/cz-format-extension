b3867e51639f62b6997379baece44b0023800199
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-08-22 01:08:30 +0900

    chore: add separator example

---
+++ example/separator/.czferc.js
@@ -0,0 +1,26 @@
+  /**
+   * @typedef {{list: string;}} Answers
+   */
+
+/** @type import('cz-format-extension').Config<Answers> */
+module.exports = {
+  questions({inquirer}) {
+    return [
+      {
+        type: 'list',
+        name: 'list',
+        choices: [
+          "choice A",
+          "choice B",
+          "choice C",
+          new inquirer.Separator(),
+          "choice D",
+        ]
+      }
+    ]
+
+  },
+  commitMessage({answers}) {
+    return answers.list
+  }
+}
---
+++ example/separator/README.md
@@ -0,0 +1,8 @@
+```bash
+? list: (Use arrow keys)
+❯ choice A
+  choice B
+  choice C
+  ──────────────
+  choice D
+```
