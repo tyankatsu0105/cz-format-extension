aacc74899a8ea48e4620583eb9b63939e7bb8fd1
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-08-29 17:19:34 +0900

    test: remove unnecessary test case

--- tests/util/gitInfo.spec.ts
+++ tests/util/gitInfo.spec.ts
@@ -1,13 +1,10 @@
-import { Git } from "../.jest/helpers";
 import { getGitInfo } from "../../src/util";
-import path from "path";
-
-const fixturePath = path.join(__dirname, "fixtures");
 
 describe("getGitInfo", () => {
   it("when run function, should return some property that helpful to use information about Git", async () => {
     const { gitInfo } = await getGitInfo();
 
+    // By git-repo-info
     expect(gitInfo).toHaveProperty("sha");
     expect(gitInfo).toHaveProperty("abbreviatedSha");
     expect(gitInfo).toHaveProperty("branch");
@@ -23,23 +20,13 @@ describe("getGitInfo", () => {
     expect(gitInfo).toHaveProperty("lastTag");
     expect(gitInfo).toHaveProperty("commitsSinceLastTag");
     expect(gitInfo).toHaveProperty("parents");
+
+    // By simple-git
     expect(gitInfo).toHaveProperty("not_added");
     expect(gitInfo).toHaveProperty("created");
     expect(gitInfo).toHaveProperty("deleted");
     expect(gitInfo).toHaveProperty("modified");
     expect(gitInfo).toHaveProperty("renamed");
     expect(gitInfo).toHaveProperty("staged");
-  });
-
-  describe("should ", () => {
-    it("should ", async () => {
-      const git = new Git(fixturePath, "repo");
-      git.init();
-
-      const { gitInfo } = await getGitInfo(git.repoPath);
-
-      expect(gitInfo).toHaveProperty("staged");
-      git.remove();
-    });
   });
 });
