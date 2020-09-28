21459ad372239f38461e413477de8e359eedf334
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-09-28 23:59:51 +0900

    feat: add isFirstCommitOnCurrentBranch

--- src/util/gitInfo.ts
+++ src/util/gitInfo.ts
@@ -1,5 +1,24 @@
 import gitRepoInfo from "git-repo-info";
 import simpleGit from "simple-git";
+import { execSync } from "child_process";
+
+const getIsFirstCommitOnCurrentBranch = () => {
+  let isFirstCommitOnCurrentBranch;
+  try {
+    const nearestBranchOrTag = execSync(
+      "git describe --contains --always --all"
+    );
+    const currentBranch = execSync("git rev-parse --abbrev-ref HEAD");
+
+    isFirstCommitOnCurrentBranch =
+      nearestBranchOrTag.toString() === currentBranch.toString();
+  } catch (error) {
+    // If not exist remote origin branch, git describe will fail and show error
+    isFirstCommitOnCurrentBranch = false;
+  }
+
+  return { isFirstCommitOnCurrentBranch };
+};
 
 export const getGitInfo = async (repoPath: string = process.cwd()) => {
   const repoInfo = gitRepoInfo(repoPath);
@@ -7,6 +26,8 @@ export const getGitInfo = async (repoPath: string = process.cwd()) => {
   const status = await gitStatus.status();
 
   const { not_added, created, deleted, modified, renamed, staged } = status;
+
+  const { isFirstCommitOnCurrentBranch } = getIsFirstCommitOnCurrentBranch();
 
   const gitInfo = {
     ...repoInfo,
@@ -16,6 +37,7 @@ export const getGitInfo = async (repoPath: string = process.cwd()) => {
     modified,
     renamed,
     staged,
+    isFirstCommitOnCurrentBranch,
   };
 
   return { gitInfo };
